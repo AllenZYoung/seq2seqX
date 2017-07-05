f35899c55254bc16a446524bc44ac730e2aced84
AllenZYoung <allenzyoungazy@gmail.com>
2017-07-05 17:19:31 +0800

    Initial commit

---
+++ theTHEANUX.py
@@ -0,0 +1,17 @@
+import theano
+import numpy
+from theano import tensor as T
+from theano import function
+
+a = T.dscalar('a')
+b = T.dscalar('b')
+
+c = a + b
+
+f = function([a,b],c)
+
+res = f(2.33, 4.5)
+print(res)
+print(f)
+print(type(a))
+
